7e1273860e2ea7e56baeb826833993c4549282a1
GetLiberatedDev <43334192+GetLiberatedDev@users.noreply.github.com>
2020-03-18 20:31:47 +0700

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA__EvfqjDJR7l0zMWTmP95txHfzODNOKc",
+    authDomain: "eris-wads-exercise4.firebaseapp.com",
+    databaseURL: "https://eris-wads-exercise4.firebaseio.com",
+    projectId: "eris-wads-exercise4",
+    storageBucket: "eris-wads-exercise4.appspot.com",
+    messagingSenderId: "844213451107"
+  }
+};
